f9f84d376219f90a976634b5be38ab79dd026185
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-02-24 09:19:34 -0300

    Checking for blank string on domain name validator

--- lib/validates_host/domain_name.rb
+++ lib/validates_host/domain_name.rb
@@ -5,7 +5,7 @@ module ValidatesHost
     end
 
     def valid?
-      return true if @domain_name.nil?
+      return true if @domain_name.blank?
       @domain_name =~ /^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
     end
 
