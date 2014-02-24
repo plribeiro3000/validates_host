eb8b3088e6a45a2ee123fb1b874d3a3af24cb807
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-02-24 08:45:52 -0300

    Fix for build on rails 4

--- lib/validates_host/shoulda-matchers.rb
+++ lib/validates_host/shoulda-matchers.rb
@@ -1,3 +1,4 @@
+require 'active_support/core_ext/array/wrap'
 require 'validates_host/shoulda-matchers/domain_name_matcher'
 require 'validates_host/shoulda-matchers/host_name_matcher'
 require 'validates_host/shoulda-matchers/ip_matcher'
